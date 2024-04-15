e7307b652feca60648884b0112689ec19643a877
Mikhail Saburov <saburov1985@gmail.com>
2024-04-15 23:11:18 +1100

    установил компонент button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="font-semibold flex items-center justify-center">
+    <main className="font-semibold flex items-center justify-center text-gray-500">
       Hello Mike!
     </main>
   );
