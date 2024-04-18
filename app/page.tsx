200a279827627e9e05c5c4d3d8cfe556d225faad
Mikhail Saburov <saburov1985@gmail.com>
2024-04-18 20:49:46 +1100

    добавил размер кнопок

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <main className="font-semibold flex gap-2 pt-3 items-center justify-center text-gray-500">
-      <Button variant="outline">Button</Button>
-      <Button>Default</Button>
-      <Button variant="destructive">Destructive</Button>
+      <Button size={'sm'} variant="outline">
+        Button
+      </Button>
+      <Button size={'default'}>Default</Button>
+      <Button size={'lg'} variant="destructive">
+        Destructive
+      </Button>
       <Button variant="primary">Primary</Button>
     </main>
   );
