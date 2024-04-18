1285fd84625fa537b442f0a21e7eca79c77b1185
Mikhail Saburov <saburov1985@gmail.com>
2024-04-18 21:32:19 +1100

    выровнил позицию элемента по центру страницы

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,8 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="font-semibold flex gap-2 pt-3 items-center justify-center text-gray-500">
-      <Button size={'sm'} variant="outline">
-        Button
-      </Button>
-      <Button size={'default'}>Default</Button>
-      <Button size={'lg'} variant="destructive">
-        Destructive
-      </Button>
-      <Button variant="primary">Primary</Button>
+    <main className="flex h-full flex-col items-center justify-center">
+      Home
     </main>
   );
 }
