b84a2a86d16a4cdb1cc3e6be93c3328e72339ab0
Mikhail Saburov <saburov1985@gmail.com>
2024-04-18 20:37:07 +1100

    feature(page): попробовал виды кнопок

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="font-semibold flex items-center justify-center text-gray-500">
-      Hello Mike!
+    <main className="font-semibold flex gap-2 pt-3 items-center justify-center text-gray-500">
+      <Button variant="outline">Button</Button>
+      <Button>Button-2</Button>
+      <Button variant="destructive">Destructive</Button>
     </main>
   );
 }
