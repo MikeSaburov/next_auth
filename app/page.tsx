17319831388b3b9d7670d0a3cf5627698b23ae7f
Mikhail Saburov <saburov1985@gmail.com>
2024-04-18 20:45:26 +1100

    рефакторинг

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="font-semibold flex gap-2 pt-3 items-center justify-center text-gray-500">
       <Button variant="outline">Button</Button>
-      <Button>Button-2</Button>
+      <Button>Default</Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="primary">Primary</Button>
     </main>
