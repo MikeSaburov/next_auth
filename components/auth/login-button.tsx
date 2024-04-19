61f39151b18d3131d568e16fd8a93cbf21dddbac
Mikhail Saburov <saburov1985@gmail.com>
2024-04-19 21:04:17 +1100

    сверстал компонент кнопки

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,7 +1,25 @@
 'use client';
 
+import { log } from 'console';
+
 interface LoginButtonProps {
   children: React.ReactNode;
   mode?: 'modal' | 'redirect';
   asChild?: boolean;
 }
+
+export const LoginButton = ({
+  children,
+  mode = 'redirect',
+  asChild,
+}: LoginButtonProps) => {
+  const onClick = () => {
+    console.log('Login Button CLICK!');
+  };
+
+  return (
+    <span className="cursor-pointer" onClick={onClick}>
+      {children}
+    </span>
+  );
+};
