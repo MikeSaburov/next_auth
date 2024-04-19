9bbdf9009bc84008f122eef1ac2c2d4f2de99edf
Mikhail Saburov <saburov1985@gmail.com>
2024-04-19 22:56:52 +1100

    добавил компонент card-wrapper

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Card } from '@/components/ui/card';
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return <Card></Card>;
+};
