ec53e6c2b202b330a0e2a9d826326a279e9c75d0
Mikhail Saburov <saburov1985@gmail.com>
2024-04-19 23:16:36 +1100

    импортировал компонент header в card-wrapper

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -12,8 +12,9 @@ interface HeaderProps {
 
 export const Header = ({ label }: HeaderProps) => {
   return (
-    <div className="w-full flex items-center justify-center flex-colgap-y-4">
+    <div className="w-full flex items-center justify-center flex-col gap-y-4">
       <h1 className={cn('text-3xl font-semibold', font.className)}> 🔐 Auth</h1>
+
       <p className="text-muted-foreground text-sm">{label}</p>
     </div>
   );
