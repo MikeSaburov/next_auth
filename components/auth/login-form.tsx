66c1024e4ed0d3025fc7eaac603afddc1e5b7dc8
Mikhail Saburov <saburov1985@gmail.com>
2024-04-21 22:06:14 +1100

    сверстал форму email

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -16,6 +16,8 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 
+import { Input } from '@/components/ui/input';
+
 export const LoginForm = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
@@ -32,7 +34,29 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="email@examle.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
