1948d0d57af76648f9e377c7e66e8facf8e51525
Mikhail Saburov <saburov1985@gmail.com>
2024-04-22 21:28:36 +1100

    добавить сообщение при некорректном вводе пароля

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,7 @@ export const LoginSchema = z.object({
   email: z.string().email({
     message: 'Некорректный адрес эл. почты',
   }),
-  password: z.string().min(3),
+  password: z.string().min(3, {
+    message: 'Пароль должен быть не менее 3 символов',
+  }),
 });
