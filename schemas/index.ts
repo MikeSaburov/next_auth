1dc2a38ef3eb47ce3f0eb1375387357e08ac0e96
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 22:10:27 +1100

    создать схему регистрации

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: 'Пароль должен быть не менее 3 символов',
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: 'Некорректный адрес эл. почты',
+  }),
+  password: z.string().min(6, {
+    message: 'Пароль должен быть не менее 6 символов',
+  }),
+  name: z.string().min(3, {
+    message: 'Имя должно быть не менее 3 символов',
+  }),
+});
