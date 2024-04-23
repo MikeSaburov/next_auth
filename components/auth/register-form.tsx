618c79210617f4fd88715586c2a79ebc02c3574a
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 22:32:06 +1100

    подключил экшен регистрации в regiser-form

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -20,7 +20,7 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { FormError } from '@/components/form-error';
 import { FormSuccess } from '@/components/form-success';
-import { login } from '@/actions/login';
+import { register } from '@/actions/register';
 
 export const RegisterForm = () => {
   const [isPending, startTransition] = useTransition();
@@ -41,7 +41,7 @@ export const RegisterForm = () => {
     setSuccess('');
 
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setError(data.error);
         setSuccess(data.success);
       });
