ff6307be8d6a789e536cfee08d535e6abaf74a4f
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 21:19:24 +1100

    добавить в action-login функции из next/cache

--- actions/login.ts
+++ actions/login.ts
@@ -1,5 +1,10 @@
 'use server';
 
+import { revalidatePath, revalidateTag } from 'next/cache';
+
 export const login = (values: any) => {
   console.log(values);
+
+  revalidatePath();
+  revalidateTag();
 };
