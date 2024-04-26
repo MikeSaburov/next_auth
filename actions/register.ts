12ddced32d0a3f45de71856e18760b37c2670cc5
Mikhail Saburov <saburov1985@gmail.com>
2024-04-26 22:08:34 +1100

    добавить создание пользователя

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
+
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
 
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -8,5 +11,30 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Упс:( Что то пошло не так, проверь!' };
   }
-  return { success: 'Успешно!' };
+
+  //вытаскиваем параметры с помощью zod
+  const { email, password, name } = validatedFields.data;
+
+  //создаем hash на пароль
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //ищем есть ли пользователь в базе с таким email
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  //если пользователь существует то ошибка
+  if (existingUser) {
+    return { error: 'Пользователь с таким email уже существует' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  return { success: 'Ползователь успешно создан' };
 };
