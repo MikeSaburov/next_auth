e9b6243c72bc6f26a2b16ae6d86a87fe676114e9
Mikhail Saburov <saburov1985@gmail.com>
2024-04-30 20:51:24 +1100

    сконфигурировал auth.ts

--- auth.ts
+++ auth.ts
@@ -2,8 +2,10 @@ import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 
+import { db } from '@/lib/db';
+
 export const { handlers, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
 });
