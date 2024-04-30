36cfdb7abe293cbf958be556f730d74366423558
Mikhail Saburov <saburov1985@gmail.com>
2024-04-30 20:47:29 +1100

    изменить auth.ts

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
+import authConfig from '@/auth.config';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+export const { handlers, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: 'jwt' },
+  ...authConfig,
 });
