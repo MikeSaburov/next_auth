83f846d39ba2a70e4bda36282c7a23cab73a5a9b
Mikhail Saburov <saburov1985@gmail.com>
2024-04-30 22:23:45 +1100

    рефакторинг auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,6 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+import { LoginSchema } from '@/schemas';
 
 export default { providers: [] } satisfies NextAuthConfig;
