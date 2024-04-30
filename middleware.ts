0117ee30b377e461295819b27ba77bcad1daf099
Mikhail Saburov <saburov1985@gmail.com>
2024-04-30 22:05:41 +1100

    рефакторинг middleware с учетом прописанных маршрутов

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,32 @@ import authConfig from './auth.config';
 import NextAuth from 'next-auth';
 
 const { auth } = NextAuth(authConfig);
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from '@/routes';
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log('IS LOGGEDIN:', isLoggedIn);
 
-  // req.auth
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+
+    return null;
+  }
 });
 
 // Optionally, don't invoke Middleware on some paths
