9d5f069d5a4bd82d6d57c4f7e32b92ad39171633
Mikhail Saburov <saburov1985@gmail.com>
2024-05-01 21:35:20 +1100

    написана логика входа в систему

--- actions/login.ts
+++ actions/login.ts
@@ -1,12 +1,32 @@
 'use server';
 import * as z from 'zod';
 
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: 'Упс:( Что то пошло не так, проверь!' };
   }
-  return { success: 'Успешно!' };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Неправильный логин или пароль' };
+        default:
+          return { error: 'Что-то пошло не так' };
+      }
+    }
+    throw error;
+  }
 };
